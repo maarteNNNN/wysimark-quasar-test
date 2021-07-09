cfb811a1664bea8c06a955f0ff1643f8e83c22e2
Maarten Coppens <martinuske@gmail.com>
2021-07-09 10:13:05 -0300

    first commit

---
+++ src/components/Wysimark.vue
@@ -0,0 +1,18 @@
+<template>
+  <Wysimark ref="wysimark" v-model="text" style="width: 100%" />
+</template>
+
+<script>
+import { ref } from "vue";
+
+import Wysimark from "wysimark-vue-3";
+
+export default {
+  setup() {
+    return {
+      text: ref(null),
+    };
+  },
+  components: { Wysimark },
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+    <Wysimark />
+    <q-btn @click="openDialog" label="Open Wysimark" />
+    <q-dialog v-model="modal"><Wysimark /></q-dialog>
+  </q-page>
+</template>
+
+<script>
+import Wysimark from "src/components/Wysimark.vue";
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  components: { Wysimark },
+  name: "PageIndex",
+  setup() {
+    const modal = ref(false);
+
+    return {
+      modal,
+      openDialog: () => modal.value = !modal.value,
+    };
+  },
+});
+</script>
