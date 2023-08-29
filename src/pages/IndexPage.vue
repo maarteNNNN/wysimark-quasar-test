0926b36ca9c09fa34309c9dd1f4df2132bda5fbe
Maarten Coppens <martinuske@gmail.com>
2023-08-29 16:15:22 +0200

    updated

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <wysimark v-model="markdown" :height="320" />
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+import wysimark from "@wysimark/vue";
+
+const markdown = ref("");
+</script>
