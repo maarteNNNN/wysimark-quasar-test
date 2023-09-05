32028247f25bb79043b08360f4e6630e1a603b91
Maarten Coppens <martinuske@gmail.com>
2023-09-05 16:44:18 +0200

    add examples for Sunny

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,20 @@
 <template>
-  <q-page class="flex flex-center">
-    <wysimark v-model="markdown" :height="320" />
+  <q-page class="flex column q-gutter-md flex-center">
+    <div>
+      <h5 class="q-ma-sm">V-Model example</h5>
+      <wysimark v-model="markdown" :height="320" />
+    </div>
+    <div>
+      <h5 class="q-ma-sm">
+        Checklist example (clicking a checkbox will log any @update:model-value
+        event in the console with the value)
+      </h5>
+      <wysimark
+        :model-value="checklistExample"
+        @update:model-value="log"
+        :height="320"
+      />
+    </div>
   </q-page>
 </template>
 
@@ -10,4 +24,9 @@ import { ref } from "vue";
 import wysimark from "@wysimark/vue";
 
 const markdown = ref("");
+const checklistExample = ref(
+  "- [ ] Test for Sunny \n - [x] Test for Sunny2 \n - [ ] Test for Sunny3"
+);
+
+const log = console.log;
 </script>
